6f4d134070f4a80b8a217010754ce58b89650a8f
asge <asge@gft.com>
2022-02-07 21:13:58 -0600

    fixin prettier

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -7,26 +7,26 @@ import {
   Body,
   Put,
   Delete,
-} from "@nestjs/common";
+} from '@nestjs/common';
 
-import { ProductsService } from "./../services/products.service";
-import { ParseIntPipe } from "./../common/parse-int.pipe";
-import { CreateProductDto } from "./../dtos/products.dtos";
-@Controller("products")
+import { ProductsService } from './../services/products.service';
+import { ParseIntPipe } from './../common/parse-int.pipe';
+import { CreateProductDto } from './../dtos/products.dtos';
+@Controller('products')
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
 
   @Get()
   getProducts(
-    @Query("limit") limit = 100,
-    @Query("offset") offset = 0,
-    @Query("brand") brand = ""
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand = '',
   ) {
     return this.productsService.findAll();
   }
 
   @Get(':productId')
-  getOne(@Param("productId", ParseIntPipe) productId: number) {
+  getOne(@Param('productId', ParseIntPipe) productId: number) {
     return this.productsService.findOne(productId);
   }
 
@@ -35,13 +35,13 @@ export class ProductsController {
     return this.productsService.create(payload);
   }
 
-  @Put(":id")
-  update(@Param("id") id: number, @Body() payload: any) {
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
     return this.productsService.update(id, payload);
   }
 
-  @Delete(":id")
-  delete(@Param("id") id: number) {
+  @Delete(':id')
+  delete(@Param('id') id: number) {
     return id;
   }
 } // END CLASS
